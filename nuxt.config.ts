b24c4618d95a554076b6a1088c47a184f2ff97f8
AmpCTM <chutimon.work@gmail.com>
2024-12-17 15:08:16 +0700

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // ...
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  // ...
+  css: ["@/assets/css/main.css"],
+
+  compatibilityDate: "2024-11-20"
+});
